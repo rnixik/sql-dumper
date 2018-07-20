ae83393fa9ca404af6bc2414b091a47a429fb70c
rNix <rnix@yandex.ru>
2018-07-20 23:50:39 +0700

    Fix type in test

--- example_writer_test.go
+++ example_writer_test.go
@@ -5,7 +5,7 @@ func Example_WriteRows() {
 	rows := make([]*map[string]interface{}, 0)
 	rows = append(rows, &map[string]interface{}{"name": "one", "title": "two"})
 	rows = append(rows, &map[string]interface{}{"id": int(123)})
-	rows = append(rows, &map[string]interface{}{"value": int(456)})
+	rows = append(rows, &map[string]interface{}{"value": int64(456)})
 	rows = append(rows, &map[string]interface{}{"amount": 1.23})
 	rows = append(rows, &map[string]interface{}{"chars": []uint8{0x26, 0x23, 0x29}})
 	rows = append(rows, &map[string]interface{}{"nulled": nil})
