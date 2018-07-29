6896136703b57eb73122e4c5a9dfc9eb90fc0b70
rNix <rnix@yandex.ru>
2018-07-29 14:46:10 +0700

    Fix fmt

--- csv_writer_test.go
+++ csv_writer_test.go
@@ -42,13 +42,13 @@ func TestCsvWriterWriteRowsToDir(t *testing.T) {
 	writer := NewCsvWriter(fw, "", "/tmp/some_dir", ";")
 	rows1 := make([]*map[string]interface{}, 0)
 	rows1 = append(rows1, &map[string]interface{}{
-		"id":    1,
-		"name":  "Name 1",
+		"id":   1,
+		"name": "Name 1",
 	})
 	rows2 := make([]*map[string]interface{}, 0)
 	rows2 = append(rows2, &map[string]interface{}{
 		"id":    1,
-		"value":  5,
+		"value": 5,
 	})
 
 	writer.WriteRows("some_table1", []string{"id", "name"}, rows1)
